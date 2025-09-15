b5e7e379acd7c6821bd2b88d4d52ea6f6d3694b4
Kaique Silva <kaikzyx@gmail.com>
2025-09-14 21:59:58 -0300

    feat: add player jump

--- source/world/player/player.gd
+++ source/world/player/player.gd
@@ -1,6 +1,7 @@
 class_name Player extends CharacterBody2D
 
 const SPEED := 100.0
+const JUMP_FORCE := 300.0
 
 func _physics_process(delta: float) -> void:
 	var movement := Input.get_axis(&"move_left", &"move_right")
@@ -8,7 +9,9 @@ func _physics_process(delta: float) -> void:
 
 	if movement != 0: $Sprite.flip_h = movement == -1
 
-	if not is_on_floor():
+	if is_on_floor():
+		if Input.is_action_just_pressed(&"jump"): velocity.y = -JUMP_FORCE
+	else:
 		velocity.y += ProjectSettings.get_setting(&"physics/2d/default_gravity") as float * delta
 
 	move_and_slide()
