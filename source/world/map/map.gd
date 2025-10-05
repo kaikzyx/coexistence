77f66563a7badba611c142cb707f7d688ddafe40
Kaique Silva <kaikzyx@gmail.com>
2025-10-04 22:32:52 -0300

    feat: change tile map appearance and modify tile_set.png

--- source/world/map/map.gd
+++ source/world/map/map.gd
@@ -6,4 +6,5 @@ func _ready() -> void:
 
 func _on_reality_changed() -> void:
 	var is_light := RealityManager.is_light_reality
+	RenderingServer.set_default_clear_color(Color(&"cccccc") if is_light else Color(&"050505")) 
 	$LightTileMap.visible = is_light; $DarkTileMap.visible = not is_light
