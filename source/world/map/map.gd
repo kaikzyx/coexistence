a0bac0761d22f88962f9ddc9179812ed973b9c8a
Kaique Silva <kaikzyx@gmail.com>
2025-11-02 18:20:21 -0300

    feat: divide the tile map into scenes, add logic so the player ignores the tiles, and change the art's color palette

--- source/world/map/map.gd
+++ source/world/map/map.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var _light_tile_map: TileMapLayer = $LightTileMap
-@onready var _dark_tile_map: TileMapLayer = $DarkTileMap
+@onready var _light_tile_map: TileMapLayer = $LightTileMap/TileMapLayer
+@onready var _dark_tile_map: TileMapLayer = $DarkTileMap/TileMapLayer
 
 func _ready() -> void:
 	Global.player_initialized.connect(_on_player_initialized)
@@ -14,10 +14,15 @@ func _update_player_state() -> void:
 	var is_light := RealityManager.is_light_reality
 	var tile_map := _light_tile_map if is_light else _dark_tile_map
 	var cell := tile_map.local_to_map(tile_map.to_local(Global.player.global_position))
+	var condition := true
+	
+	var data := tile_map.get_cell_tile_data(cell)
+	if data and data.has_custom_data(&"ignorable"):
+		condition = not data.get_custom_data(&"ignorable")
 
 	# Retuns to the backup position in case it collides with the world when changing reality.
 	# Otherwise it just saves a new backup position.
-	if Global.player.hitbox.has_overlapping_bodies() or tile_map.get_cell_source_id(cell) == -1:
+	if Global.player.hitbox.has_overlapping_bodies() or condition:
 		Global.player.return_to_backup_position()
 	else:
 		Global.player.save_backup_position()
