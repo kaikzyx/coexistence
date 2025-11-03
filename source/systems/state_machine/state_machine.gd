7832f96185f4b27063db5cc2432da57b10e1cfcd
Kaique Silva <kaikzyx@gmail.com>
2025-11-02 21:05:00 -0300

    feat: add state machine system

---
+++ source/systems/state_machine/state_machine.gd
@@ -0,0 +1,42 @@
+class_name StateMachine extends Node
+
+signal state_changed(from: State, to: State)
+
+@export var initial_state: State
+
+var active := true
+var _current_state: State
+
+func _process(delta: float) -> void:
+	if active and is_instance_valid(_current_state):
+		_current_state.processed.emit(delta)
+
+func _physics_process(delta: float) -> void:
+	if active and is_instance_valid(_current_state):
+		_current_state.physics_processed.emit(delta)
+
+func start() -> void:
+	assert(is_instance_valid(initial_state), "The state machine needs an initial state.")
+
+	# Initialize.
+	_current_state = initial_state
+
+	if active:
+		_current_state.entered.emit()
+		state_changed.emit(null, _current_state)
+
+func request_state(state: StringName) -> void:
+	var new_state := get_node_or_null(state.to_pascal_case() + &"State") as State
+
+	if is_instance_valid(new_state) and new_state != _current_state:
+		_current_state = new_state
+
+		if active:
+			var previous := _current_state
+			previous.exited.emit()
+			_current_state.entered.emit()
+			state_changed.emit(previous, _current_state)
+
+func get_current_state() -> StringName:
+	if not is_instance_valid(_current_state): return &"";
+	return _current_state.get_state_name()
